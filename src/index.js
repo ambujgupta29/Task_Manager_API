3c69c31bdb00224fe2247b74976fb16ebdf756f0
AMBUJ <ambujdggupta@gmail.com>
2021-05-10 15:28:33 +0530

    Init Commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require('express')
+require('./db/mongoose')
+const userrouter=require('./routers/user')
+const taskrouter=require('./routers/task')
+
+const app=express()
+const port=process.env.PORT
+
+app.use(express.json())
+app.use(userrouter)
+app.use(taskrouter)
+
+app.listen(port,()=>{
+    console.log('server is up on port '+ port)
+})
+
+
+
+
